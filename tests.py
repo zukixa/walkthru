6a00472cc28ad5cd440879b1a0e8fa8aca38297d
zukixa <56563509+zukixa@users.noreply.github.com>
2024-05-24 13:12:34 -0700

    RED: test_day_in_next_month added.

--- tests.py
+++ tests.py
@@ -5,6 +5,8 @@ class TestGetUSDateFunction(unittest.TestCase):
         self.assertEqual(get_us_date(0), 'January 1, 1960')
     def test_day_count_after_start(self):
         self.assertEqual(get_us_date(10), 'January 11, 1960')
+    def test_day_in_next_month(self):
+        self.assertEqual(get_us_date(32), 'February 2, 1960')
         
 if __name__ == '__main__':
     unittest.main()
