b70232d5ba5dc0abe1e96be04ec9c5919ea65bc0
zukixa <56563509+zukixa@users.noreply.github.com>
2024-05-24 13:04:21 -0700

    REFACTOR: changes applied.

--- implementation.py
+++ implementation.py
@@ -1,2 +1,12 @@
 def get_us_date(day_count):
+    """
+    Get the US date corresponding to the given day count.
+
+    Parameters:
+        day_count (int): The number of days since January 1, 1960.
+
+    Returns:
+        str: The US date corresponding to the given day count.
+
+    """
     return 'January 1, 1960'
