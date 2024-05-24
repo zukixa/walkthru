8dd7f2d82879a268cbe1d65f0e53b0fd88b9b123
zukixa <56563509+zukixa@users.noreply.github.com>
2024-05-24 12:53:36 -0700

    RED: test_day_count_at_start added.

--- tests.py
+++ tests.py
@@ -1,4 +1,8 @@
 import unittest
 from implementation import get_us_date
 class TestGetUSDateFunction(unittest.TestCase):
-    pass
+    def test_day_count_at_start(self):
+        self.assertEqual(get_us_date(0), 'January 1, 1960')
+        
+if __name__ == '__main__':
+    unittest.main()
