693570e7625569c6efdc4c20a10320d8d26e3902
zukixa <56563509+zukixa@users.noreply.github.com>
2024-05-26 15:03:32 -0700

    GREEN: test_for_leap_year_transition passes.

--- implementation.py
+++ implementation.py
@@ -1,3 +1,12 @@
+def is_leap_year(year):
+        if year % 4 == 0:
+            if year % 100 == 0:
+                if year % 400 == 0:
+                    return True
+                return False
+            return True
+        return False
+    
 def get_us_date(day_count):
     """
     Get the US date corresponding to the given day count.
@@ -11,8 +20,12 @@ def get_us_date(day_count):
     """
     current_day, current_month, current_year = 1, 1, 1960
     months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
-    month_days = [31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+    month_days = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
     while day_count > 0:
+        if is_leap_year(current_year):
+            month_days[1] = 29
+        else:
+            month_days[1] = 28
         days_in_current_month = month_days[current_month - 1]
         days_till_month_end = days_in_current_month - current_day + 1
         
