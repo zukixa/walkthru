7a10b59e2de5d7b6171977bd00c932bc84740684
zukixa <56563509+zukixa@users.noreply.github.com>
2024-05-26 14:53:45 -0700

    RED: test_for_leap_year_transition added.

--- tests.py
+++ tests.py
@@ -9,6 +9,8 @@ class TestGetUSDateFunction(unittest.TestCase):
         self.assertEqual(get_us_date(32), 'February 2, 1960')
     def test_day_in_next_year_during_leap(self):
         self.assertEqual(get_us_date(366), 'January 1, 1961')
+    def test_for_leap_year_transition(self):
+        self.assertEqual(get_us_date(425), 'March 1, 1961')
         
 if __name__ == '__main__':
     unittest.main()
