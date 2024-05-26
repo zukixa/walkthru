8e95a0123b8354fc42e165ff402596513fe61d4e
zukixa <56563509+zukixa@users.noreply.github.com>
2024-05-26 14:38:13 -0700

    GREEN: test_day_in_next_month passes.

--- implementation.py
+++ implementation.py
@@ -9,5 +9,20 @@ def get_us_date(day_count):
         str: The US date corresponding to the given day count.
 
     """
-    init_day = 1
-    return f'January {init_day + day_count}, 1960'
+    init_day, init_month = 1, 1
+    months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
+    month_days = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+    while day_count > 0:
+        days_in_current_month = month_days[init_month - 1]
+        days_till_month_end = days_in_current_month - init_day + 1
+        
+        if day_count >= days_till_month_end:
+            day_count -= days_till_month_end
+            init_month += 1
+            init_day = 1
+        else:
+            init_day += day_count
+            day_count = 0
+
+        
+    return f'{months[init_month - 1]} {init_day}, 1960'
